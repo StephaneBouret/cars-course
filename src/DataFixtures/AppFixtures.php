f110c53225ad5df4b3afcdecbea6728898088c87
Stéphane BOURET <sbouret@orange.fr>
2023-11-09 11:20:41 +0100

    Mise en place de l'entité User et de la Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        $admin = new User;
+
+        $hash = $this->passwordHasher->hashPassword($admin, "password");
+
+        $admin->setEmail("admin@gmail.com")
+            ->setFirstname("Vincent")
+            ->setLastname("Parot")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName())
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
