edb45f54a2c7d49e966cf628634c9b25384017fc
Stéphane BOURET <sbouret@orange.fr>
2023-11-15 10:31:26 +0100

    Mise en place des pages d'erreurs

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
+    .copyFiles({
+        from: './assets/img',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
     /*
      * ENTRY CONFIG
      *
